c40d472cf153aca4fe0216582351d37ac10787a6
DESKTOP-NN9HF7C\k0322 <k0322123@gmail.com>
2023-02-21 00:04:57 +0900

    MH_CODE

---
+++ pcCafe/src/pcCafe/main/MemberView.java
@@ -0,0 +1,20 @@
+package pcCafe.main;
+
+public class MemberView {
+	
+
+	    public String getInput(String prompt){
+	        System.out.print(prompt + ":");
+	        return ScannerUtil.SC.nextLine();
+	    }
+
+	    public String showMessage(String message) {
+	        System.out.println(message);
+	        return ScannerUtil.SC.nextLine();
+	    }
+
+	    public  void showError(String error) {
+	        System.err.println(error);
+	    }
+
+}
---
+++ pcCafe/src/pcCafe/main/ScannerUtil.java
@@ -0,0 +1,8 @@
+package pcCafe.main;
+
+import java.util.Scanner;
+
+public class ScannerUtil {
+
+	    public static final Scanner SC = new Scanner(System.in);
+}
