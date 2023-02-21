a1364f48b1c63e4accde82eab87d5d534f0297ed
DESKTOP-NN9HF7C\k0322 <k0322123@gmail.com>
2023-02-21 17:27:01 +0900

    COMMIT 추가

--- pcCafe/src/pcCafe/useStatus/ServiceManager.java
+++ pcCafe/src/pcCafe/useStatus/ServiceManager.java
@@ -119,12 +119,12 @@ public class ServiceManager {
 		        ResultSet rs = pstmt.executeQuery();
 
 		        // 결과집합에서 데이터 꺼내기
-		        while (rs.next()) {
-		            int seatNumber = rs.getInt("SEAT_NUM");
+		        while(rs.next()){
+		            int seatNum = rs.getInt("SEAT_NUM");
 		            String SEAT_TYPE = rs.getString("SEAT_TYPE");
 		            String USAGE_YN = rs.getString("USAGE_YN");
 
-		            System.out.print(seatNumber + "번" + " ◻︎ ");
+		            System.out.print(seatNum + "번" + " ◻︎ ");
 		            System.out.print(" || ");
 
 		            if (SEAT_TYPE.equals("금연")) {
@@ -178,20 +178,22 @@ public class ServiceManager {
 		                showSeat();
 		            } else {
 		                System.out.println(seatNum + "번 좌석을 선택하셨습니다.");
-		        PreparedStatement pstmt2 = conn.prepareStatement(sql2);
-		        pstmt2.setInt(1,seatNum);
-		        pstmt2.setInt(2,MemberMenu.memberNum);
-		        pstmt2.executeUpdate();
-		        //좌선 선택함과 동시에 좌석테이블 사용 여부 업데이트
-		        PreparedStatement pstmt3 = conn.prepareStatement(sql3);
-		        pstmt3.setInt(1,seatNum);
-		        int result2 = pstmt3.executeUpdate();
-        		        if(result2 ==1) {
-        		        	System.out.println("이용이 시작되었습니다.");
-        		        	ssm.afterChooseSeat();
-        		        } else {
-        		        	System.out.println("이용 실패");
-        			    }
+				        PreparedStatement pstmt2 = conn.prepareStatement(sql2);
+				        pstmt2.setInt(1,seatNum);
+				        pstmt2.setInt(2,MemberMenu.memberNum);
+				        int result1 = pstmt2.executeUpdate();
+				        conn.commit();
+				        //좌선 선택함과 동시에 좌석테이블 사용 여부 업데이트
+				        PreparedStatement pstmt3 = conn.prepareStatement(sql3);
+				        pstmt3.setInt(1,seatNum);
+				        int result2 = pstmt3.executeUpdate();
+				        conn.commit();
+		        		        if(result2 ==1) {
+		        		        	System.out.println("이용이 시작되었습니다.");
+		        		        	ssm.afterChooseSeat();
+		        		        } else {
+		        		        	System.out.println("이용 실패");
+		        			    }
 		            }
 		          
 		        // conn 정리
